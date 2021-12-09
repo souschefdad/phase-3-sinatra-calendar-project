998e1a4edc062504c06af9a5ddf1da072bced121
Pierce <piercedavidraphael@gmail.com>
2021-12-08 20:03:17 -0600

    Displaying an Event

--- app/calendar-frontend/src/App.js
+++ app/calendar-frontend/src/App.js
@@ -1,12 +1,34 @@
 
+import {useState, useEffect} from 'react';
 import './App.css';
 import Form from './Form';
 import Sidebar from './Sidebar';
 
 function App() {
+  const [events, setEvents] = useState([])
+
+  useEffect(() => {
+    fetch("http://localhost:9292/events_list")
+    .then(resp => resp.json())
+    .then(setEvents)
+  },[])
+
   return (
+    
     <div className="App">
-       <Sidebar/>
+
+      {events.map((date_event) =>{
+        return (
+          <>
+          <h1>Name: {date_event.name}</h1>
+          <h2>Country: {date_event.country}</h2>
+          <h2>Location: {date_event.location}</h2>
+          <h2>Type: {date_event.holiday_type}</h2>
+          </>
+        )
+      })}
+
+      <Sidebar/>
       <h1>Calendar</h1>
       <Form/>
     </div>
